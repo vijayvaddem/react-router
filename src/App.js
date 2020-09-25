0f3fe724c4b1af173d761b283dfb0634f7b9b95b
Vijay Vaddem <vijay.vaddem@gmail.com>
2020-09-26 00:50:07 +0530

    Adding routing logic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
+
 import "./App.css";
 import Feed from "./components/Feed";
 import Profile from "./components/Profile";
+import { HomePage } from "./components/HomePage";
+import { ProfileDetail } from "./components/ProfileDetail";
 
 function App() {
   return (
     <div>
-      <Feed />
-      <Profile />
+      <Router>
+        <ul>
+          <li>
+            <Link to="/">To Home</Link>
+          </li>
+          <li>
+            <Link to="/profile">To Profile</Link>
+          </li>
+          <li>
+            <Link to="/profile/andrew" exact>
+              To Andrew's Profile
+            </Link>
+          </li>
+          <li>
+            <Link to="/feed">To Feed</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/profile" component={Profile} />
+          <Route path="/profile/:username" component={ProfileDetail} />
+          <Route path="/feed" component={Feed} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/ProfileDetail.js
+++ src/components/ProfileDetail.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
-export default function () {
-  const username = useParams();
+export function ProfileDetail() {
+  const { username } = useParams();
   return (
     <div>
       <h1>Profile detail page for {username}</h1>
