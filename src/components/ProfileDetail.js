018932aa2a304633586614509aecfc700ac957db
Vijay Vaddem <vijay.vaddem@gmail.com>
2020-09-25 23:58:39 +0530

    setting up router project

---
+++ src/components/ProfileDetail.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+export default function () {
+  const username = useParams();
+  return (
+    <div>
+      <h1>Profile detail page for {username}</h1>
+    </div>
+  );
+}
